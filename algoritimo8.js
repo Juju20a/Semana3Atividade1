174522416802d37c8f0c9149f209467bb4fcb2cf
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-11-13 10:21:29 -0300

    algoritimo8.js

--- algoritimo8.js
+++ algoritimo8.js
@@ -1,1 +1,12 @@
+const users = [ // Criação de um array de objetos 'users'.
 
+{ firstName: "john", lastName: "colben", age: 26 },
+{ firstName: "jimmy", lastName: "fred", age: 75 },
+{ firstName: "sam", lastName: "boston", age: 50 },
+{ firstName: "ronald", lastName: "bristh", age: 26 },
+
+];
+
+const output = users.filter((x) => x.age < 30).map((x) => x.firstName); // Filtra usuários com idade abaixo de 30, e mapeia para um array com seus primeiros nomes.
+
+console.log(output); // Exibe o array de primeiros nomes dos usuários que têm menos de 30 anos. Resultado esperado: ["john", "ronald"].
