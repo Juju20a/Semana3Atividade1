303c20dbd332932d879607c296541abcdb6969a2
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-11-13 10:13:47 -0300

    algoritimo2.js

--- algoritimo2.js
+++ algoritimo2.js
@@ -1,1 +1,8 @@
+const numbers = [1, 2, 3, 4, 5]; // Cria um array de números
 
+const output = numbers.map((x) => { // aqui é ultilizado o método map na variavel já criada numbers
+
+return x * 2;// Retorna o valor de 'x' multiplicado por 2.
+});
+
+console.log(output); //Exibe no console o resultado de output que contém o resultado de map
