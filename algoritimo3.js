d95c13fbf77ed51f721364274c8c8dbca05f76e8
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-11-13 10:14:37 -0300

    algoritimo3.js

--- algoritimo3.js
+++ algoritimo3.js
@@ -1,1 +1,5 @@
+const numbers = [1, 2, 3, 4, 5]; // Criação de um array de números.
+const isOdd = (x) => x % 2 !== 0; // verifica se o número é ímpar.
+const output = numbers.filter(isOdd); // Aqui o códico usa o filter para filtrar os números ímpares do array numbers
 
+console.log(output); //Exibe no console o resultado da filtragem
