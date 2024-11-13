8bc96a8e56f0d2b833a83220e3b948ec1da79f56
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-11-13 10:15:22 -0300

    algoritimo4.js

--- algoritimo4.js
+++ algoritimo4.js
@@ -1,1 +1,5 @@
+const arr = [1, 2, 3, 4, 5]; // Criação de um array de números.
 
+const output = arr.filter((x) => x > 4); // Aplica o filtro no array 'arr', selecionando apenas os números maiores que 4.
+
+console.log(output); // Exibe no console o resultado do filtro, que será um array com os valores [5].
